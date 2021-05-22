c2a6bc1b5ae96c80f688ddddccc55f38a73d0190
Sandip Wane <swane@cloudhedge.io>
2021-05-23 00:13:17 +0530

    APIs for Generating Short URL and Redirection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const helpers = require("../src/helpers/index");
 
 // Show homepage
 router.get("/", function (_req, res) {
-  res.render("index", { title: "Chotu URL" });
+  res.render("index", { title: "URL Shortner" });
+});
+
+// create a short url
+router.post("/api/shorten", function (req, res) {
+  const fullUrl = req.body.url;
+  const shortUrl = helpers.saveShortUrl(fullUrl);
+  const hostName = req.headers.host;
+  res.send({
+    message: "URL shortened successfully",
+    data: {
+      url: `${hostName}/${shortUrl}`,
+    },
+  });
+});
+
+// decode short url and redirect
+router.get("/:shortUrl", function (req, res) {
+  const hostName = req.headers.host;
+  const shortUrl = req.params.shortUrl;
+  const fullUrl = helpers.getFullUrlByShortUrl(shortUrl);
+  if (!fullUrl) {
+    res.redirect(`${hostName}/`);
+  } else {
+    res.redirect(fullUrl);
+  }
 });
 
 module.exports = router;
