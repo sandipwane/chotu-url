25097da15c925e8b2766009e73f65fda82cbed9d
Sandip Wane <swane@cloudhedge.io>
2021-05-23 00:11:27 +0530

    Generate Short URL and Fetch Short URL

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,32 @@
 // write code here
+const { nanoid } = require("nanoid");
+
+const CODE_LENGTH = 6;
+
+const db = [];
+
+function generateShortIdForUrl() {
+  return nanoid(CODE_LENGTH);
+}
+
+function saveShortUrl(fullUrl) {
+  const shortUrl = generateShortIdForUrl();
+
+  const doc = db.find((doc) => doc.fullUrl === fullUrl);
+  if (doc) {
+    return doc.shortUrl;
+  }
+  db.push({ fullUrl, shortUrl });
+
+  return shortUrl;
+}
+
+function getFullUrlByShortUrl(shortUrl) {
+  const doc = db.find((doc) => doc.shortUrl === shortUrl);
+  return doc.fullUrl;
+}
+
+module.exports = {
+  saveShortUrl,
+  getFullUrlByShortUrl,
+};
