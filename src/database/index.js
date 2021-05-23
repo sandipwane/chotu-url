42f467dcce8ad13b144dc5846a5519142c7b967d
Sandip Wane <swane@cloudhedge.io>
2021-05-23 13:37:14 +0530

    Fix NeDB File Corrupt Issue

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const Datastore = require("nedb-promises");
 const { join } = require("path");
 const file = join(__dirname, "..", "..", "db.json");
-const datastore = Datastore.create(file);
+const datastore = Datastore.create({fileName: file, corruptAlertThreshold: 1 });
 
 module.exports = datastore;
